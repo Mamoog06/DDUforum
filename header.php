49959fc7eeaa0a9bc24acfca1c0b58158d9cf084
Marcus_Hansenberg <marc453b@mail.hansenberg.dk>
2024-10-08 11:56:52 +0200

    Tilføjet en header og en footer
    header og footer der vil være i toppen og bunden

---
+++ header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" 
+"https://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="nl" lang="nl">
+<head>
+</head>
+<body>
+<h1>Forum</h1>
+	<div id="wrapper">
+	<div id="menu">
+		<a class="item" href="index.php">Home</a> -
+		<a class="item" href="/DDUforum/create_post.php">Create a post</a>
+
+		<div id="userbar">
+		<div id="userbar">Hello user. Log out.</div>
+	</div>
+		<div id="content">
